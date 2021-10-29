bb5d582798c9cb5acc5e5b842b6a3acc45b11b92
ankitgoel25 <ankitoct01@gmail.com>
2021-10-30 01:06:55 +0530

    Logo component updated

--- src/component/Logo.jsx
+++ src/component/Logo.jsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import styles from '../styles/Home.module.scss';
 
 export default function Logo({ classes }) {
   return (
-    <div className={`${styles.Logo} ${classes}`}>
-      <img src="/images/logo-illus.png" alt="logo" />
-      <img src="/images/logo-title.png" alt="title" />
-    </div>
+    <Link href="/">
+      <div className={`${styles.Logo} ${classes}`}>
+        <img src="/images/logo-illus.png" alt="logo" />
+        <img src="/images/logo-title.png" alt="title" />
+      </div>
+    </Link>
   );
 }
