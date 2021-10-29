7f849cce2b98ea629b1a78ce01514d66e7bf5945
Disha Bhardwaj <disha2.bhardwaj@gmail.com>
2021-10-30 01:34:18 +0530

    Integrated Login Modal

--- src/component/LoginModal/LoginModal.jsx
+++ src/component/LoginModal/LoginModal.jsx
@@ -1,37 +1,47 @@
-import { useState } from 'react';
-import styles from '../../styles/LoginModal.module.scss';
-import { Modal, Button } from 'antd';
-import Register from './Register';
-import Login from './Login';
+import { useState } from "react";
+import styles from "../../styles/LoginModal.module.scss";
+import { Modal, Button } from "antd";
+import Register from "./Register";
+import Login from "./Login";
 
 const LoginModal = () => {
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [toggleLogin,setToggleLogin]= useState(true);
-    const showModal = () => {
-        setIsModalVisible(true);
-      };
-    
-      const handleOk = () => {
-        setIsModalVisible(false);
-      };
-    
-      const handleCancel = () => {
-        setIsModalVisible(false);
-      };
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [toggleLogin, setToggleLogin] = useState(true);
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
 
-      return (
-        <>
-          <Button type="primary" onClick={showModal}>
-            Get Started
-          </Button>
-          <Modal visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} footer={null}>
-            {toggleLogin? <Login /> : <Register /> }
-            <div className={styles.switchUser}>
-                <p>New User?  <span onClick={()=>setToggleLogin(!toggleLogin)}>{toggleLogin ? 'Register' : 'Login'}</span> </p>
-            </div>
-          </Modal>
-        </>
-      );
-}
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
 
-export default LoginModal
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <div className={styles.authBtns}>
+      <Button type="primary" onClick={showModal}>
+        Get Started
+      </Button>
+      <Modal
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        {toggleLogin ? <Login /> : <Register />}
+        <div className={styles.switchUser}>
+          <p>
+            New User?
+            <span onClick={() => setToggleLogin(!toggleLogin)}>
+              {toggleLogin ? "Register" : "Login"}
+            </span>
+          </p>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default LoginModal;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,29 @@
-import Head from 'next/head';
-import LoginModal from '../component/LoginModal/LoginModal';
-import styles from '../styles/Home.module.scss'; 
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
+import Logo from "../component/Logo";
+import LoginModal from "../component/LoginModal/LoginModal";
+import { Button } from "antd";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.homePage}>
       <Head>
         <title>Home Page</title>
       </Head>
-      <div>
-        <LoginModal />
-      </div>
+      <section className={styles.main}>
+        <Logo />
+        <div className={styles.content}>
+          <p>
+            Lorem ipsum dolor sit ametidsm consectetur adipisicing elit.
+            Voluptatem, maxime.
+          </p>
+          <LoginModal />
+        </div>
+      </section>
+      <section className={styles.heroSection}>
+        <div className={styles.overlay} />
+        <iframe src="https://embed.lottiefiles.com/animation/36605"></iframe>
+      </section>
     </div>
   );
 }
