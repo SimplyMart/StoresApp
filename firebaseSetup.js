cf71edb96267e6dc3d9275010105a301c1fd5d0c
ankitgoel25 <ankitoct01@gmail.com>
2021-10-29 21:45:48 +0530

    Firebase Setup updated

--- firebaseSetup.js
+++ firebaseSetup.js
@@ -5,12 +5,12 @@ import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  // databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  // measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
