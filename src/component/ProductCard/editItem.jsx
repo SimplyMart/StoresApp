b44896a637901e21d85c90cfa3d8c44f933457f1
Disha Bhardwaj <disha2.bhardwaj@gmail.com>
2021-10-30 23:19:12 +0530

    Added states in edit-item form

--- src/component/ProductCard/editItem.jsx
+++ src/component/ProductCard/editItem.jsx
@@ -1,8 +1,14 @@
 import { Modal, Button } from "antd";
 import { useState } from "react";
 import styles from "../../styles/component/editItem.module.scss";
-const EditItem = () => {
+const EditItem = ({ name, price, stock }) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [details, setDetails] = useState({
+    name: " ",
+    price: " ",
+    stock: " ",
+  });
+
   const showModal = () => {
     setIsModalVisible(true);
   };
@@ -10,6 +16,26 @@ const EditItem = () => {
   const handleCancel = () => {
     setIsModalVisible(false);
   };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setDetails((prevDetail) => ({
+      ...prevDetail,
+      [name]: value,
+    }));
+  };
+
+  const submitEditForm = async (event) => {
+    event.preventDefault();
+
+    const { name, price, stock } = details;
+
+    try {
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   return (
     <div className={styles.editContainer}>
       <Button className={styles.editButton} onClick={showModal}>
@@ -19,6 +45,7 @@ const EditItem = () => {
         title="EDIT"
         centered
         visible={isModalVisible}
+        onCancel={handleCancel}
         footer={[
           <Button key="back" onClick={handleCancel}>
             Cancel
@@ -26,21 +53,45 @@ const EditItem = () => {
         ]}
       >
         <div className={styles.editForm}>
-          <form>
+          <form onSubmit={submitEditForm}>
             <div className={styles.editFormDetail}>
               <label htmlFor="name">Name</label>
-              <input id="name" type="text" placeholder="Item Name" required />
+              <input
+                id="name"
+                name="name"
+                type="text"
+                placeholder="Item Name"
+                value={details.name}
+                onChange={handleChange}
+                required
+              />
             </div>
             <div className={styles.editFormDetail}>
               <label htmlFor="price">Price</label>
-              <input id="price" type="Number" placeholder="Price" required />
+              <input
+                id="price"
+                type="Number"
+                name="price"
+                value={details.price}
+                onChange={handleChange}
+                placeholder="Price"
+                required
+              />
             </div>
             <div className={styles.editFormDetail}>
               <label htmlFor="stock">Stock</label>
-              <input id="stock" type="Number" placeholder="Stock" required />
+              <input
+                id="stock"
+                name="stock"
+                type="Number"
+                value={details.stock}
+                onChange={handleChange}
+                placeholder="Stock"
+                required
+              />
             </div>
             <div className={styles.editFormButton}>
-              <Button type="primary">Save Changes</Button>
+              <button type="submit">Save Changes</button>
             </div>
           </form>
         </div>
