55c7771451d025476700a8b7bdc9b8ea50917143
Disha Bhardwaj <disha2.bhardwaj@gmail.com>
2021-11-07 11:02:23 +0530

    fixed product add form image height

--- src/component/Dashboard/Products/addItem.jsx
+++ src/component/Dashboard/Products/addItem.jsx
@@ -1,4 +1,4 @@
-import styles from '../../../styles/component/addItem.module.scss';
+import styles from "../../../styles/component/addItem.module.scss";
 import {
   Typography,
   Upload,
@@ -7,13 +7,13 @@ import {
   Input,
   Button,
   message,
-} from 'antd';
-import { useState } from 'react';
-import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
-import { useAuth } from '../../../utils/context/AuthUserContext';
-import { updateDoc, doc, arrayUnion } from '@firebase/firestore';
-import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
-import { db, storage } from '../../../utils/firebase';
+} from "antd";
+import { useState } from "react";
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
+import { useAuth } from "../../../utils/context/AuthUserContext";
+import { updateDoc, doc, arrayUnion } from "@firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { db, storage } from "../../../utils/firebase";
 
 const { Title } = Typography;
 
@@ -26,10 +26,10 @@ export default function AddProductItem() {
   const [form] = Form.useForm();
   const [submitLoading, setSubmitLoading] = useState(false);
   const [itemInfo, setItemInfo] = useState({
-    prodId: '',
-    itemName: '',
-    price: '',
-    stock: '',
+    prodId: "",
+    itemName: "",
+    price: "",
+    stock: "",
   });
   const [uploadState, setUploadState] = useState({
     loading: false,
@@ -42,23 +42,23 @@ export default function AddProductItem() {
   };
 
   function beforeUpload(file) {
-    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+    const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
     if (!isJpgOrPng) {
-      message.error('You can only upload JPG/PNG file!');
+      message.error("You can only upload JPG/PNG file!");
     }
     const isLt2M = file.size / 1024 / 1024 < 2;
     if (!isLt2M) {
-      message.error('Image must smaller than 2MB!');
+      message.error("Image must smaller than 2MB!");
     }
     return isJpgOrPng && isLt2M;
   }
 
   const handleUploadChange = (info) => {
-    if (info.file.status === 'uploading') {
+    if (info.file.status === "uploading") {
       setUploadState({ loading: true });
       return;
     }
-    if (info.file.status === 'done') {
+    if (info.file.status === "done") {
       // Get this url from response in real world.
       const file = info.file.originFileObj;
       Object.assign(file, { preview: window.URL.createObjectURL(file) });
@@ -88,18 +88,18 @@ export default function AddProductItem() {
     if (uploadState.file) {
       const snapshot = await uploadBytes(
         ref(storage, `/stores/products/${prodId}`),
-        uploadState.file,
+        uploadState.file
       );
       const downloadURL = await getDownloadURL(snapshot.ref);
       console.log(downloadURL);
       image = downloadURL;
     } else {
-      message.error('Please add a product image!');
+      message.error("Please add a product image!");
       setSubmitLoading(false);
       return;
     }
 
-    await updateDoc(doc(db, 'store', authUser.uid), {
+    await updateDoc(doc(db, "store", authUser.uid), {
       products: arrayUnion({
         prodId,
         itemName: name,
@@ -117,25 +117,26 @@ export default function AddProductItem() {
     });
     updateStoreData({ products });
     setSubmitLoading(false);
-    message.success('Item added successfully!');
+    message.success("Item added successfully!");
     onReset();
   };
 
   return (
     <div className={styles.additem}>
       <div className={styles.newItemIllus}>
-        <img src="/images/addNewItem.png"></img>
+        <img src='/images/addNewItem.png'></img>
       </div>
       <Title level={1}>
         ADD A NEW ITEM
         <Divider />
       </Title>
       <div className={styles.addItemForm}>
-        <Form form={form} layout="vertical" onFinish={handleFormSubmit}>
+        <Form form={form} layout='vertical' onFinish={handleFormSubmit}>
           <Upload
-            name="avatar"
-            listType="picture-card"
+            name='avatar'
+            listType='picture-card'
             style={{ marginBottom: 150 }}
+            className='imageUpload'
             showUploadList={false}
             beforeUpload={beforeUpload}
             onChange={handleUploadChange}
@@ -143,8 +144,8 @@ export default function AddProductItem() {
             {uploadState.file ? (
               <img
                 src={uploadState.file.preview}
-                alt="avatar"
-                style={{ width: '100%' }}
+                alt='avatar'
+                style={{ width: "100%", height: "100%" }}
               />
             ) : (
               uploadButton
@@ -152,8 +153,8 @@ export default function AddProductItem() {
           </Upload>
           <Form.Item
             className={styles.formInput}
-            label="Product Id"
-            name="prodId"
+            label='Product Id'
+            name='prodId'
             value={itemInfo.prodId}
             onChange={handleFormFields}
             rules={[
@@ -162,12 +163,12 @@ export default function AddProductItem() {
               },
             ]}
           >
-            <Input placeholder="Enter Product Id" type="number" />
+            <Input placeholder='Enter Product Id' type='number' />
           </Form.Item>
           <Form.Item
             className={styles.formInput}
-            label="Product Name"
-            name="name"
+            label='Product Name'
+            name='name'
             value={itemInfo.itemName}
             onChange={handleFormFields}
             rules={[
@@ -176,12 +177,12 @@ export default function AddProductItem() {
               },
             ]}
           >
-            <Input placeholder="Enter Product Name" />
+            <Input placeholder='Enter Product Name' />
           </Form.Item>
           <Form.Item
             className={styles.formInput}
-            label="Price"
-            name="price"
+            label='Price'
+            name='price'
             value={itemInfo.price}
             onChange={handleFormFields}
             rules={[
@@ -190,12 +191,12 @@ export default function AddProductItem() {
               },
             ]}
           >
-            <Input type="Number" placeholder="Enter price per piece" />
+            <Input type='Number' placeholder='Enter price per piece' />
           </Form.Item>
           <Form.Item
             className={styles.formInput}
-            label="Stock"
-            name="stock"
+            label='Stock'
+            name='stock'
             value={itemInfo.stock}
             onChange={handleFormFields}
             rules={[
@@ -204,25 +205,25 @@ export default function AddProductItem() {
               },
             ]}
           >
-            <Input type="Number" placeholder="Enter total quantity" />
+            <Input type='Number' placeholder='Enter total quantity' />
           </Form.Item>
           <Form.Item>
             <div className={styles.formButtons}>
               <div>
                 <Button
                   loading={submitLoading}
-                  type="primary"
-                  htmlType="submit"
-                  className="normalBtn"
+                  type='primary'
+                  htmlType='submit'
+                  className='normalBtn'
                 >
                   Submit
                 </Button>
               </div>
               <div>
                 <Button
-                  htmlType="button"
+                  htmlType='button'
                   onClick={onReset}
-                  className="normalBtn"
+                  className='normalBtn'
                 >
                   Reset
                 </Button>
